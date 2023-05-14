5f41ee689f01c4b6c16679889659716c3c4c28b3
RAFAEL ORIOL ORTI CARRIO <raorcar3@gmail.com>
2023-05-14 17:53:13 +0200

    Geekshubs nodejs template initialized

---
+++ db/dbconnect.js
@@ -0,0 +1,21 @@
+
+const dbconnect = () => {
+
+    //dB connection//////////
+    const mongoose = require("mongoose");
+    const conn_str = "mongodb+srv://new_user_36:1pUu9uuXQqQiqGfK@dbhive.cu5o7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+    mongoose.connect(
+        conn_str,
+        { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        },(err) => {
+            if (err) {
+                console.log("error in connection",err);
+            } else {
+                console.log("mongodb is connected");
+        }});
+}
+
+module.exports = dbconnect;
---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const app=express();
+const cors = require('cors');
+const router = require('./router');
+const port = 3000;
+
+let corsOptions = {//CONFIGURO OPCIONES DE CORS
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
+//Middleware
+app.use(express.json()); //PUEDO OBTENER JSON DEL BODY
+app.use(cors(corsOptions));  //USO CORS
+
+app.use(router);
+
+//db connection
+const dbconnect = require('./db/dbconnect');
+
+dbconnect();
+
+app.listen(port, () => console.log(`Node server runing on http://localhost:${port}` ))
